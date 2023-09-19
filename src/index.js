1b39253947e7ed0a080cfc7bd318213959b1e1c0
lance-malaga <lmalaga@my.bcit.ca>
2023-09-19 14:14:23 -0700

    add components to App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import news from './data/sample_news_stories.json'
+import './styles.css';
+
+// components
+import FeaturedNews from './components/FeaturedNews';
+import News from './components/News';
+
+function App() {
+  const data = news.results;
+  const featured_news = data[0];
+
+  return (
+    <main className="main_container">
+      <h1>News Feed</h1>
+      <hr/>
+      <FeaturedNews
+        image={featured_news.image_url}
+        title={featured_news.title}
+        creator={featured_news.creator}
+        description={featured_news.description}
+        link={featured_news.link}
+      />
+      <hr/>
+      <div className="news_container">
+        {data.slice(1).map((article, index) => {
+          return (
+            <News 
+              key={index}
+              image={article.image_url}
+              title={article.title}
+              creator={article.creator}
+              description={article.description}
+              category={article.category}
+              link={article.link}
+            />
+          )
+        })}
+      </div>
+    </main>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
